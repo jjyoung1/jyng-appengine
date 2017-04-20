31d15cf3d0d8c8cf1a6913d7af2f14040371ef04
Joel Young <joel@jyng.com>
2017-04-20 17:47:32 -0400

    rot13 appears to be functional

--- rot13.py
+++ rot13.py
@@ -1,15 +1,44 @@
 import os
 import jinja2
 import webapp2
-
+import string
 from handler import Handler
 
-class Rot13Handler(Handler):
 
+# rot13_table = set(string.ascii_letters)
+# rot13_table_UC = [range(ord('A'),ord('Z'))]
+# rot13_table += rot13_table_UC
+
+# import string
+# s = set(string.ascii_lowercase)
+# s = set()
+#   s = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}
+
+class Rot13Handler(Handler):
     def get(self):
-        self.render("rot13.html")
+        text = "Please enter some text here"
+        self.render("rot13.html", text=text)
 
     def post(self):
         text = self.request.get("text")
-        self.render("rot13.html")
+        text = self.encode(text)
+        self.render("rot13.html", text=text)
 
+    def encode(self, text):
+        obfr = ""
+        for char in text:
+            # Only convert alpha characters
+            if char.isalpha():
+                # Determine base for wrap-around management
+                if char.islower():
+                    base = ord('a')
+                else:
+                    base = ord('A')
+
+                # Convert the character
+                c = ord(char) - base
+                c += 13
+                c = c%26 + base
+                char = chr(c)
+            obfr+=char
+        return obfr
