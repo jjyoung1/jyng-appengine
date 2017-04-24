3920e13fd5056a55469bae96686027af6fa11d74
Joel Young <joel@jyng.com>
2017-04-24 05:33:05 -0400

    Removed map from bound parameters

--- user_signup.py
+++ user_signup.py
@@ -2,18 +2,15 @@ from handler import Handler
 
 
 class UserSignupHandler(Handler):
-    def _init_ctx(self):
-        ctx = {"username": "","password": "","verify": ""}
-        return ctx
 
     def get(self):
-        ctx = self._init_ctx();
-        self.render('user_signup.html', ctx=ctx)
+        self.render('user_signup.html')
 
     def post(self):
         username = self.request.get("username")
         password = self.request.get("password")
         verify = self.request.get("verify")
-        self.render('user_signup.html', ctx={"username": username,
-                                             "password": password,
-                                             "verify": verify})
+        self.render('user_signup.html',
+                    username=username,
+                    password=password,
+                    verify=verify)
