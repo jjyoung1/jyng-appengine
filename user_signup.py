d2a599f1f95cddebc1a48f6cc6a4643e03a21bac
Joel Young <joel@jyng.com>
2017-04-25 13:17:26 -0400

    Additional cleanup of validation routines and dictionary setup for bound variables

--- user_signup.py
+++ user_signup.py
@@ -2,81 +2,73 @@ import re
 
 from handler import Handler
 
-class UserSignupHandler(Handler):
-    USER_RE = re.compile("^[a-zA-Z0-9_-]{3,20}$")
-    PASSWORD_RE = re.compile("^.{3,20}$")
-    EMAIL_RE = re.compile("^[\S]+@[\S]+.[\S]+$")
 
+class UserSignupHandler(Handler):
     def get(self):
         self.render('user_signup.html')
 
     # TODO: Cleanup context passing to template
     def post(self):
+        # get posted parameters
         username = self.request.get("username")
         password = self.request.get("password")
         verify = self.request.get("verify")
         email = self.request.get("email")
-        valid_username = self._validate_user(username)
-        valid_password = self._validate_password(password)
-        valid_verify = self._validate_verify(password, verify)
-        valid_email = self._validate_email(email)
+
+        # Initialize context for response
+        # We don't want the password fields repopulated in case of error
+        ctx = dict(username=username,
+                   email=email)
+
+        # Initialize verification error status to false
+        error = False
+
+        # If invalid username setup error and message
+        if not _valid_user(username):
+            ctx['username_error'] = 'Invalid Username'
+            error = True
+
+        if not _valid_password(password):
+            ctx['password_error'] = 'Invalid Password'
+            error = True
+
+        if not _valid_password(verify) or password != verify:
+            ctx['verify_error'] = "Passwords don't match"
+            error = True
 
         # Check if all inputs are valid
-        if (valid_username and valid_password and valid_verify and valid_email):
-            url = '/welcome?username='+valid_username
+        if not error:
+            url = '/welcome?username=' + username
             self.redirect(url)
 
         else:
-            username_error = ""
-            password_error = ""
-            verify_error = ""
-            email_error = ""
-
-            if not valid_username:
-                username_error = "Invalid Username"
-            if not valid_password:
-                password_error = "Invalid Password"
-            if not valid_verify:
-                verify_error = "Passwords do not match"
-            if not valid_email:
-                email_error = "Email address is invalid"
-
+            # set error response code
             self.response.status_int = 400
+            self.render('user_signup.html', **ctx)
 
-            self.render('user_signup.html',
-                        username=username,
-                        password=password,
-                        verify=verify,
-                        email=email,
-                        username_error=username_error,
-                        password_error=password_error,
-                        verify_error=verify_error,
-                        email_error=email_error
-                        )
 
-    def _validate_user(self, username):
-        if UserSignupHandler.USER_RE.match(username):
-            return username
-        else:
-            return None
+#
+# Utility functions used for parameter verification
+#
+USER_RE = re.compile("^[a-zA-Z0-9_-]{3,20}$")
 
-    def _validate_password(self, password):
-        if UserSignupHandler.PASSWORD_RE.match(password):
-            return password
-        else:
-            return None
 
-    def _validate_verify(self, password, verify):
-        if UserSignupHandler.PASSWORD_RE.match(verify) and verify==password:
-            return verify
-        else:
-            return None
+def _valid_user(username):
+    return username and USER_RE.match(username)
 
-    def _validate_email(self, email):
-        if UserSignupHandler.EMAIL_RE.match(email):
-            return email
-        else:
-            return None
+
+PASSWORD_RE = re.compile("^.{3,20}$")
+
+
+def _valid_password(password):
+    return password and PASSWORD_RE.match(password)
+
+
+EMAIL_RE = re.compile("^[\S]+@[\S]+.[\S]+$")
+
+
+def _valid_email(self, email):
+    return not email or EMAIL_RE.match(email)
 
 
 class WelcomeHandler(Handler):
